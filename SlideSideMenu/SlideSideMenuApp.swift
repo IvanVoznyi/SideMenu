3cced223a1a0e285b2463a4c96e22a0a5c491e79
Ivan Voznyi <ivan.voznyi92@gmail.com>
2025-09-24 15:32:57 +0300

    Initial Commit

---
+++ SlideSideMenu/SlideSideMenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  SlideSideMenuApp.swift
+//  SlideSideMenu
+//
+//  Created by Ivan Voznyi on 9/24/25.
+//
+
+import SwiftUI
+
+@main
+struct SlideSideMenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
